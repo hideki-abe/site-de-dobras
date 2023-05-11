4fd763417d247c673ba393dd434173f24734b283
hidekiabe <hideki-abe@hotmail.com>
2023-05-11 17:53:32 -0300

    att

--- app/page.js
+++ app/page.js
@@ -27,8 +27,17 @@ export default function Home() {
   aba_resultado: 0
  }
 
+ const valorInicialDobraU = {
+  dobrau_a: 0,
+  dobrau_b: 0,
+  dobrau_c: 0,
+  dobrau_espessura: 0,
+  dobrau_resultado: 0
+ }
+
  const [valor, setValor] = useState(valorInicialDobraUOuZ)
  const [valor2, setValor2] = useState(valorInicialDobraAba)
+ const [valor3, setValor3] = useState(valorInicialDobraU)
 
  function handleChangeA(event) {
   const { name, value } = event.target;
@@ -40,25 +49,33 @@ export default function Home() {
   setValor2({...valor2, [name]: value});
  }
 
-  const submitB = () => {
-    const desconto = parseInt(valor2.aba_a) + parseInt(valor2.aba_b) + 
-    parseInt(valor2.aba_c) + parseInt(valor2.aba_d)  - parseInt(8*(valor2.aba_espessura))
-    console.log(desconto)
-    setValor({...valor2, ["resultado"]: desconto})
-  }
+ function handleChangeC(event) {
+  const { name, value } = event.target;
+  setValor3({...valor3, [name]: value});
+ }
 
-  const submitA = () => {
-    const desconto = parseInt(valor.a) + parseInt(valor.b) + parseInt(valor.c) - parseInt(4*(valor.d))
-    console.log(desconto)
-    setValor({...valor, ["resultado"]: desconto})
-  }
+ const submitA = () => {
+  const desconto = parseInt(valor.a) + parseInt(valor.b) + parseInt(valor.c) - parseInt(4*(valor.d))
+  setValor({...valor, ["resultado"]: desconto})
+}
+
+const submitB = () => {
+  const desconto = parseInt(valor2.aba_a) + parseInt(valor2.aba_b) + 
+                    parseInt(valor2.aba_c) + parseInt(valor2.aba_d)  - parseInt(8*(valor2.aba_espessura))
+  setValor2({...valor2, ["aba_resultado"]: desconto})
+}
+
+const submitC = () => {
+  const desconto = parseInt(valor3.dobrau_a) + parseInt(valor3.dobrau_b) + 
+                    parseInt(valor3.dobrau_c) - parseInt(4*(valor3.dobrau_espessura))
+  setValor3({...valor2, ["aba_resultado"]: desconto})
+}
 
     return (
-      <main style={{marginTop: '1em', marginLeft: '1em'}}>
-        <div style={{border: '1px solid #000'}}>
-          <h1>DOBRA Z OU U</h1>
+      <main style={{marginTop: '3em', marginLeft: '1em'}}>
+        <div className='box'>
+          <h2>DOBRA Z OU U</h2>
           <Image src={dobraz} style={{height:'200px', width:'300px'}}></Image>
-          <Image src={dobrau} style={{height:'180px', width:'250px', marginLeft:'2em'}}></Image>
           <form style={{marginTop: '20px'}}>
             <div>
               <label>Medida A: </label>
@@ -104,8 +121,8 @@ export default function Home() {
           </button>
           <h2>RESULTADO: {valor.resultado}</h2>
           </div>
-          <div>
-            <h1>DOBRA U COM ABA</h1>
+          <div className='box'>
+            <h2>DOBRA U COM ABA</h2>
             <Image src={dobraaba} style={{height:'230px', width:'300px'}}></Image>
             <form>
             <div>
@@ -160,6 +177,54 @@ export default function Home() {
           CALCULAR
           </button>
           <h2 style={{marginBottom: '2em'}}>RESULTADO: {valor2.aba_resultado}</h2>
+          </div>
+          <div className='box'>
+            <h2>DOBRA U SEM ABA</h2>
+            <Image src={dobrau} style={{height:'230px', width:'300px'}}></Image>
+            <form>
+            <div>
+              <label>Medida A: </label>
+              <input 
+              name="dobrau_a"
+              type="number"
+              onChange={e => handleChangeC(e)}>
+              </input>
+            </div>
+            <br></br>
+            <div>
+              <label>Medida B: </label>
+              <input
+              name="dobrau_b" 
+              type={'number'}
+              onChange={e => handleChangeC(e)}>
+              </input>
+            </div>
+            <br></br>
+            <div>
+              <label>Medida C: </label>
+              <input
+              name="dobrau_c" 
+              type={'number'}
+              onChange={e => handleChangeC(e)}>
+              </input>
+            </div>
+            <br></br>
+            <div>
+              <label>Espessura da chapa: </label>
+              <input 
+              type={'number'}
+              name="dobrau_espessura"
+              onChange={e => handleChangeC(e)}>
+              </input>
+            </div>
+          </form>
+          <button 
+          style={{marginTop: '1em'}}
+          onClick={submitC}
+          >
+          CALCULAR
+          </button>
+          <h2 style={{marginBottom: '2em'}}>RESULTADO: {valor3.dobrau_resultado}</h2>
           </div>
       </main>
     )
